569c1f23f337e553af67e179a789f990ee2e3a74
VILLOSTAS DV <villostas.dv@obmontessori.edu.ph>
2026-04-17 13:24:47 +0800

    yea

---
+++ main.js
@@ -0,0 +1,18 @@
+function parkCount(){
+    let carCounter = 18;
+    const maxCars = 25;
+
+    while (carCounter < maxCars) {
+        window.alert("Car no." + carCounter + "is entering");
+        carCounter += 1
+
+    }
+
+    while (carCounter = maxCars) {
+        window.alert("Full Parking")
+
+
+    }
+
+
+}
